e6677ee02a0ec4c7eca92fc252ff523ca660ebe3
mohammedsallam812@gmail.com <mohammedsallam812@gmail.com>
2023-10-18 23:00:17 +0300

    InfrastructureRegisteration

---
+++ Ecom.Infrastructure/InfrastructureRegisteration.cs
@@ -0,0 +1,20 @@
+﻿using Ecom.Core.Interfaces;
+using Ecom.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure
+{
+    public static class InfrastructureRegisteration
+    {
+        public static IServiceCollection InfrastructureConfigration(this  IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            return services;
+        }
+    }
+}
