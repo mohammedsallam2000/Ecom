350b4d819683b44564b440ff63b37ddd475cdb38
mohammedsallam812@gmail.com <mohammedsallam812@gmail.com>
2023-10-19 00:36:36 +0300

    ApplicationDbContext Configration

--- Ecom.Infrastructure/InfrastructureRegisteration.cs
+++ Ecom.Infrastructure/InfrastructureRegisteration.cs
@@ -1,5 +1,8 @@
 ﻿using Ecom.Core.Interfaces;
+using Ecom.Infrastructure.Data;
 using Ecom.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -11,12 +14,18 @@ namespace Ecom.Infrastructure
 {
     public static class InfrastructureRegisteration
     {
-        public static IServiceCollection InfrastructureConfigration(this  IServiceCollection services)
+        public static IServiceCollection InfrastructureConfigration(this  IServiceCollection services,IConfiguration configuration)
         {
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             //services.AddScoped<ICategoryRepository,CategoryRepository>();
             //services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            // Configare DB
+            services.AddDbContext<ApplicationDbContext>(opt =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
             return services;
         }
     }
