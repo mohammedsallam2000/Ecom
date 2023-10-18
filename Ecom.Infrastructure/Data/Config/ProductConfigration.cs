7cc8f3f1c0295860c04898ab98b55447305de84a
mohammedsallam812@gmail.com <mohammedsallam812@gmail.com>
2023-10-18 22:13:02 +0300

    GenericRepository Interface And Configration

---
+++ Ecom.Infrastructure/Data/Config/ProductConfigration.cs
@@ -0,0 +1,21 @@
+﻿using Ecom.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure.Data.Config
+{
+    public class ProductConfigration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Id).IsRequired();
+            builder.Property(x => x.Name).HasMaxLength(50);
+            builder.Property(x => x.Price).HasColumnType("decimal (18,2)");
+            builder.HasOne(x => x.Category).WithMany().HasForeignKey(x => x.CategoryId);
+        }
+    }
+}
---
+++ Ecom.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using Ecom.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public GenericRepository()
+        {
+                
+        }
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(params Expression<Func<T, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(T id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T id, T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
